a8e38809ab0aa77255d28f6de24f33e772a8c791
Ilanss <jeremie.provenzano@gmail.com>
2019-05-08 19:31:42 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 
 function api() {
 
-
     return fetch('https://api.themoviedb.org/3/movie/550?api_key=d227582077145ec762e4105f563380ac')
         .then(res => res.json())
         .then(function (data) {
