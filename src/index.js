3e11bbb97cd278b438cb3351da5c5214a2de7a2d
ilanss <jeremie_pro@hotmail.com>
2019-05-10 16:02:10 +0200

    Adding data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import "billboard.js";
+import "bootstrap";
+import {APIKEY} from "./key";
 
-function api() {
+let baseURL = 'https://api.themoviedb.org/3/';
 
-    return fetch('https://api.themoviedb.org/3/movie/550?api_key='.key)
-        .then(res => res.json())
-        .then(function (data) {
+let fetchAll = function () {
+    let url = ''.concat(baseURL, 'discover/movie?api_key=', APIKEY);
+    fetch(url)
+        .then(result=>result.json())
+        .then((data)=>{
             console.log(data);
 
+            var vo = map()
+
+
+            var chart = bb.generate({
+                data: {
+                    columns: [
+                        ["data1", 30, 200, 100, 400, 150, 250],
+                        ["data2", 130, 100, 140, 200, 150, 50]
+                    ],
+                    type: "bar"
+                },
+                bar: {
+                    width: {
+                        ratio: 0.5
+                    }
+                },
+                bindto: "#barChart"
+            });
+
+            setTimeout(function() {
+                chart.load({
+                    columns: [
+                        ["data3", 130, -150, 200, 300, -200, 100]
+                    ]
+                });
+            }, 1000);
+
+
+
+
+            //process the returned data
+            document.getElementById('output').innerHTML = JSON.stringify(data, null, 4);
+            //work with results array...
 
         })
-        .catch(err => console.error(err));
 }
 
-data = api();
-
-console.log(data);
+fetchAll();
